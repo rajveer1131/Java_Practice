8b41d03326a72ac8c0848e569810fae58ea53c90
rajveer1131 <95412132+rajveer1131@users.noreply.github.com>
2025-10-04 05:47:06 +0000

    Java Practice 1

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main{
+    public static void Main(Strings [] args){
+        Scanner sc = new Scanner();
+
+        int n = sc.nextLine;
+
+        if(n%2!=0){
+            System.out.println("ODD");
+        }
+        else{
+            System.out.println("EVEN");
+        }
+    }
+}
